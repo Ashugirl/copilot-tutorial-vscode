9a2ad4023a02c05f0e32d27184bbf517b0ff527e
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2023-07-21 11:49:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember()) {
+    const member = {
+        name: 'John',
+        age: 25,
+        skills: ['JavaScript', 'CSS', 'HTML'],
+        calculateNumbers: function(var1, var2) {
+            const sum = var1 + var2
+            return sum
+        }
+    }
+    return member
+}
