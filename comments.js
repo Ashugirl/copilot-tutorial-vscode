b2f9fff437e0733668cfe6ae72defb1347090132
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2023-07-21 11:56:59 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,57 @@
+// Create web server
+// Path: server.js
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World')
+// })
+// server.listen(3000)
+// console.log('Server is listening on port 3000')
+// Path: server.js
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World')
+// })
+// server.listen(3000)
+// console.log('Server is listening on port 3000')
+// Path: server.js
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World')
+// })
+// server.listen(3000)
+// console.log('Server is listening on port 3000')
+// Path: server.js
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World')
+// })
+// server.listen(3000)
+// console.log('Server is listening on port 3000')
+// Path: server.js
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World')
+// })
+// server.listen(3000)
+// console.log('Server is listening on port 3000')
+// Path: server.js
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World')
+// })
+// server.listen(3000)
+// console.log('Server is listening on port 3000')
+// Path: server.js
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World')
+// })
+// server.listen(3000)
+// console.log('Server is listening on port 3000')
+// Path: server.js
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World')
+// })
+// server.listen(3000)
+// console.log('Server is listening on port 3000')
